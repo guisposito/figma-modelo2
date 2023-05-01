526fba0e29d1c861054f92cf4231c53ae24619ae
guisposito <guicarlosjoinhas@gmail.com>
2023-05-01 14:41:50 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+      <h1 className="text-3xl bg-black font-bold underline">
+      Hello world!
+    </h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
