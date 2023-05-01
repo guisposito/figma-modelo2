f6907e2c592a84dcac090f5423d7039397777be4
guisposito <guicarlosjoinhas@gmail.com>
2023-05-01 18:32:58 -0300

    End Menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,37 @@
 const Header = () => {
+    const title="The Coffe Roasters";
+    const subtitle = "We Source our coffee seasonally, from farmers who care, and roast to highlight what makes them special."
+    
     return (
-        <header>
-            <h1 className="bg-black">Title</h1>
-        </header>
+        <header className="text-white font-inter">
+            <div className="bg-hero-pattern h-[1032px] bg-cover bg-no-repeat bg-center">
+                <div className="flex flex-row justify-between items-center container mx-auto pt-12">
+                    <div className="">
+                        <h2 className="hover:underline underline-offset-4">CAFÉ DO DOIDO</h2>
+                    </div>
+                    <ul className="flex justify-between gap-10 ">
+                        <li className="hover:underline underline-offset-4"><a href="">HOME</a></li>
+                        <li className="hover:underline underline-offset-4"><a href="">WHY US</a></li>
+                        <li className="hover:underline underline-offset-4"><a href="">MENU</a></li>
+                        <li className="hover:underline underline-offset-4"><a href="">HAPPY CLIENTS</a></li>
+                        <li className="hover:underline underline-offset-4"><a href="">BLOGS</a></li>
+                        <li className="hover:underline underline-offset-4"><a href="">OUR LOCATIONS</a></li>
+                        <li className="hover:underline underline-offset-4"><a href="">NEWSLETTER</a></li>
+                    </ul>
+                </div>
+                <div className="">
+                    <div className=""></div>
+                    <div className="">
+                        <h1>{title}</h1>
+                        <p>{subtitle}</p>
+                    </div>
+                    <div className="">
+                        <a href=""></a>
+                    </div>
+                </div>
+            </div>
+       </header>
     );
 }
 
-export default Header;
+export default Header; 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'inter': ['Inter Regular', 'sans-serif'],
+      },
+      backgroundImage: {
+        'hero-pattern': "url('/img/hero.jpg')",
+      }
+    },
   },
   plugins: [],
 }
