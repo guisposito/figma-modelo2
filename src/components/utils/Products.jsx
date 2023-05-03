3da78d5244e411c043c769ebaf736082f94ab06d
GuiSposito <guicarlosjoinhas@gmail.com>
2023-05-03 13:20:02 -0300

    Ajustado json de produto e componente de produtos

--- src/components/utils/Products.jsx
+++ src/components/utils/Products.jsx
@@ -2,11 +2,12 @@ const Products = ({products}) => {
     return(
         <div className="">
              {products.map(product => (
-                <div className="font-josefins text-ft-grey-100 w-[300px] relative " key={product.name}>
+                <div className="font-josefins text-ft-grey-100 w-[300px] relative " key={product.id}>
                     <a href="#">
-                        <img className="rounded" src={"../img/products/" + product.img} alt={product.alt} width="300" height="383" />
+                        <img className="rounded" src={product.img} alt={product.alt} width="300" height="383" />
                         <h4 className="mt-[20px] text-[20px]">{product.brand} {product.name}</h4>
-                        <p className="mt-1 mb-[40px] font-semibold">Preço: R$ {product.price}</p>
+                        <p className="mt-1 mb-[40px] font-semibold">{product.description}</p>
+                        <span className="mt-1 mb-[40px] font-semibold">R$ {product.price}</span>
                     </a>
                     
                 </div>
