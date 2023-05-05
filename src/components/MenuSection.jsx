8b25219ce79799e5fc6402da54e7a45bc4e772ab
guisposito <guicarlosjoinhas@gmail.com>
2023-05-05 20:35:00 -0300

    Implementado filtro funcional nos produtos

--- src/components/MenuSection.jsx
+++ src/components/MenuSection.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import HeadingSection from "./utils/HeadingSection";
 import Products from "./utils/Products";
 import Data from "../data/products.json";
 
 const MenuSection = () => {
-  const handleClick = (event) => {
+  const [filter, setFilter] = useState('Foods');
+  
+  const handleFilterChange = (event) => {
     event.preventDefault();
-  };
+    const newFilter = event.target.innerText;
+    setFilter(newFilter);
+  }
+
+  const filteredProducts = Data.filter((product)=>{
+    return product.category === filter;
+  })
+
+
   return (
     <section>
       <div className="h-[912px] bg-bg-section">
@@ -19,15 +30,15 @@ const MenuSection = () => {
         </div>
         <div className="container mx-auto mt-12 h-[528px]">
           <ul className="flex items-center justify-center gap-10">
-            <li className="hover:underline" onClick={handleClick}>
-              <a href="">FOOD</a>
+            <li className={`hover:underline ${filter === 'Foods' ? 'underline' : ''}`} onClick={handleFilterChange}>
+              <a href="">Foods</a>
             </li>
-            <li className="hover:underline" onClick={handleClick}>
-              <a href="">DRINKS</a>
+            <li className={`hover:underline ${filter === 'Drinks' ? 'underline' : ''}`} onClick={handleFilterChange}>
+              <a href="">Drinks</a>
             </li>
           </ul>
           <div className="container mx-auto mt-12">
-            <Products products={Data} />
+            <Products products={filteredProducts} />
           </div>
         </div>
       </div>
